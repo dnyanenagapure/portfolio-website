7b58de8addf85affed649ce4c804c0ccbe26ff15
dnyanenagapure <dnyanenagapure@gmail.com>
2025-02-19 16:38:25 +0530

    add changes

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -12,8 +12,10 @@ const Hero = () => {
       <h1><span>I'm Dnyaneshwari Nagapure,</span> a professional frontend developer.</h1>
       <p>I am a Frontend Developer with over 3.5 years of experience, specializing in React.js, and have a proven track record of delivering high-quality front-end solutions across multiple companies.</p>
       <div className="hero-action">
-        <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
-        <div className="hero-resume">My resume</div>
+        <div className="nav-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
+        <div className="hero-resume">
+          <a href="/Resume.pdf" download="Dnyaneshwari_Nagapure.pdf">My resume</a>
+        </div>
       </div>
     </div>
   )
